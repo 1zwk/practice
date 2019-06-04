e8962a0905881554b0d9fd771cbc280f4517fc7e
S ZHANG <459617255@qq.com>
2019-06-04 21:24:19 +0800

    5

--- 顺序表-链表/LinkNode2.java
+++ 顺序表-链表/LinkNode2.java
@@ -7,7 +7,7 @@ class Node{
 	}	
 	
 }
-
+/*
 public class LinkNode2{
 	//创造链表
 	public static Node creatLink(){
@@ -70,3 +70,104 @@ public class LinkNode2{
 		
 	}
 }
+*/
+
+
+class Solution{
+	public Node creatLink(){
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(2);
+		Node n4 = new Node(3);
+		Node n5 = new Node(4);
+		
+		n1.next = n2;n2.next = n3;n3.next = n4;n4.next = n5;n5.next = null;
+		return n1;
+	}
+	public Node findKthToTail(Node head,int k){
+		Node cur = head;
+		int count = 0;
+		while(cur != null){
+			cur = cur.next;
+			count++;
+		}
+		if(k < count){
+			System.out.println("没有第k个元素");
+			return null;
+		}
+		int n = count - k;
+		Node kth = head;
+		for(int i = 0; i<n; i++){
+			kth = kth.next;
+		}
+		return kth;	
+	}
+	public Node findKthToTail2(Node head,int k){
+		Node frond = head;
+		Node back = head;
+		
+		for(int i = 0; i < k; i++){
+			if(frond == null){
+				return null;
+			}
+			frond = frond.next;
+		}
+		
+		while(frond != null){
+			frond = frond.next;
+            back = back.next;			
+		}
+		return back;
+	}	
+	
+	//结合有序链表
+	public Node mergeTwoLists(Node L1, Node L2){
+		Node cur1 = L1;
+		Node cur2 = L2;
+		Node result = null;
+		
+		while(cur1 != null && cur2 != null){
+			if(cur1.val <= cur.val){
+				//把cur1尾插到result上
+				if(result == null){
+					Node newNode = new Node(cur1.val);
+					newNode.next = result;
+					result = newNode;
+				}else{
+					Node last = null;					
+					Node last = cur1.next;
+					cur1 = cur1.next;	
+                    last.next = newNode; 					
+				}
+			}
+		}	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void displayLink(Node head){
+		for(Node cur = head; cur != null; cur = cur.next){
+			System.out.printf("%d-->",cur.val);
+		}
+		System.out.println("null");    
+}
+
+public class LinkNode2{
+	public static void main(String[] args){
+		Solution a = new Solution();
+		Node L1 = a.creatLink();
+		Node L2 = a.creatLink();
+		a.mergeTwoLists(L1, L2);
+		
+	}
