b8ea6cbe686b1174950f0093a9041efda4ff5665
S ZHANG <459617255@qq.com>
2019-06-18 23:42:31 +0800

    1

---
+++ 链表面试题/DeleteAllVal.java
@@ -0,0 +1,29 @@
+class Node{
+	int val;
+	Node next;
+	
+	public Node(int val){
+		this.val = val;
+		this.next = next;
+	}
+}
+
+class Solution{
+	public Node deleteALLVal(Node head, int val){
+		if(head == null){
+			System.out.println("错");
+		}
+		Node cur = head;
+		Node prev = new Node(0);
+		prev.next = head;
+		while(cur != null){
+			if(cur.val == val){
+				prev.next = cur.next;
+			}else{
+				prev = prev.next;
+			}
+			cur = cur.next;
+		}
+		return head;
+	}
+}
