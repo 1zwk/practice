625570bfaa26280254cc6d3fc62ce6e417accab4
S ZHANG <459617255@qq.com>
2019-06-19 00:04:28 +0800

    3

--- 链表面试题/DeleteAllVal.java
+++ 链表面试题/DeleteAllVal.java
@@ -34,3 +34,30 @@ class Solution{
 		System.out.println("null");
     }
 }
+
+public class DeleteAllVal{
+	public static void testDeleteAllVal(){
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		Node n6 = new Node(5);
+		
+		n1.next = n2;
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = null;
+		
+		Solution s = new Solution();
+		Node result = s.deleteALLVal(n1, 5);
+		s.display(result);
+	}
+	
+	public static void main(String[] args){
+		testDeleteAllVal();
+	}
+}
