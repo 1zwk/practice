83b459a7f77e0ffd88bf1bc8a06f6b969299b2f6
S ZHANG <459617255@qq.com>
2019-06-20 23:42:54 +0800

    1

---
+++ 链表面试题/FindK.java
@@ -0,0 +1,26 @@
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+public ListNode FindKth(ListNode head,int k) {
+		int length = 0;
+		for (ListNode cur = head; cur != null; cur = cur.next) {
+			length++;
+		}
+		
+		if (length < k) {
+			return null;
+		}
+		
+		int n = length - k;
+		ListNode kth = head;
+		for (int i = 0; i < n; i++) {
+			kth = kth.next;
+		}
+		
+		return kth;
+    }
