e6872a7f22fce2ef811a51df7a8e878d7c736727
S ZHANG <459617255@qq.com>
2019-07-14 23:31:01 +0800

    1

---
+++ 练习/src/AdvanceOdd.java
@@ -0,0 +1,20 @@
+public class AdvanceOdd {
+    public int[] advanceOdd(int[] array){
+        int front = 0;
+        int back = array.length - 1;
+        while(front < back){
+            if(front % 2 == 0){
+               if(back % 2 == 1){
+                   int tmp = array[front];
+                   array[front] = array[back];
+                   array[back] = tmp;
+               }
+            }
+            front++;
+            back--;
+        }
+        return array;
+    }
+}
+
+
