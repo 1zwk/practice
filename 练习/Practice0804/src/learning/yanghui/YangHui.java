4d1e931d515d3dc2c1af27246d0f3975d8b4cd9a
S ZHANG <459617255@qq.com>
2019-08-06 15:45:18 +0800

    1

---
+++ 练习/Practice0804/src/learning/yanghui/YangHui.java
@@ -0,0 +1,34 @@
+package learning.yanghui;
+/*
+ * 
+ **杨辉三角**
+ * 
+ */
+
+public class YangHui {
+   public static void main(String[] args) {
+	   //1.创建并初始化一个二维数组
+	   int[][] yangHui = new int[10][];
+	   for(int i = 0; i < yangHui.length; i++) {
+		   yangHui[i] = new int[i+1];
+	   	   
+	   //2.赋值
+		   //2.1给首末元素赋值
+	       yangHui[i][0] = yangHui[i][i] = 1; 
+	       //2.2给非首末元素赋值
+	       if(i > 1) {
+	    	   for(int j = 1; j < yangHui[i].length-1; j++) {
+	    		   yangHui[i][j] = yangHui[i-1][j-1] + yangHui[i-1][j];
+	    	   }
+	       }
+	   }
+	  
+	   //3.遍历
+	   for(int i = 0; i < yangHui.length; i++) {
+		   for(int j = 0; j < yangHui[i].length; j++) {
+			   System.out.print(yangHui[i][j] + " ");
+		   }
+		   System.out.println();
+	   }
+   }
+}
