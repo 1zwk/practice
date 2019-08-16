5f2321b426475d8bab9a6de93a3cec21a26fab28
S ZHANG <459617255@qq.com>
2019-08-16 15:20:29 +0800

    3

--- 练习/Practice/src/learn0815/AddDeleteNode.java
+++ 练习/Practice/src/learn0815/AddDeleteNode.java
@@ -5,16 +5,15 @@ class Node{
 	int val;
 	Node next;
 
-	Node(){
-		this.val = val;
-	}
-	
 	Node(int val){
 		this.val = val;
 	}
+    @Override
+	public String toString() {
+    	return String.format("Node(%d)",val);
+    }
 
 }
-
 
 public class AddDeleteNode {
 	public Node creatList() {
@@ -22,9 +21,12 @@ public class AddDeleteNode {
 		Node n2 = new Node(2);
 		Node n3 = new Node(3);
 		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		Node n6 = new Node(6);
+		Node n7 = new Node(7);
 		
-		
-		n1.next = n2;n2.next = n3;n3.next = n4;n4.next = null;
+		n1.next = n2;n2.next = n3;n3.next = n4;n4.next = n5; n5.next = n6;
+		n6.next = n7;n7.next = null;
 		
 		return n1;
 	}
