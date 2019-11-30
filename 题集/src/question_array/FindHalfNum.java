02e36b4fd4f581b580cc378b9ee249845cc26e18
S ZHANG <459617255@qq.com>
2019-11-30 21:21:59 +0800

    1

--- 题集/src/question_array/FindHalfNum.java
+++ 题集/src/question_array/FindHalfNum.java
@@ -1,10 +1,25 @@
 package question_array;
+/**
+ * 标题：n个数里出现次数大于等于n/2的数
+ * 【n个数里出现次数大于等于n/2的数】输入n个整数，输出出现次数大于等于数组长度一半的数。
+ */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 思路一：
+ * 使用“抵消”的思想。如果这个数大于或者等于这组数的一半，那么用这个数和不同于这个数的数相抵消，
+ * 那么剩下的这个数数量一定大于等于0；但是最后要把这个数带进数组检查一下，因为如果前面都抵消了
+ * 只剩下最后一个数或者最后两个不同得数，也可能为count也可能为0。
+ * 思路二：
+ * 将整个数组排序，既然出现次数大于等于一半，那么下标n/2的位置一定就是这个数
+ * （因为题目说明一定有这个数，如果存在没有的可能那就不可以用这个思路）
+ * 优化：可以借用快排做partition的思想来写
+ */
 public class FindHalfNum {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
@@ -25,7 +40,49 @@ public class FindHalfNum {
             if (count == 0) {
                 temp = a[i];
                 count++;
-            } else {
+            } else if(temp == a[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (temp == a[i]) {
+                count++;
+            }
+        }
+        if(count >= a.length/2){
+            return temp;
+        }
+        return Integer.MAX_VALUE;
+    }
+}
+
+/*class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[100];
+        int i = 0;
+        while(sc.hasNext()){
+            a[i++] = sc.nextInt();
+        }
+        int result = find(a);
+        System.out.println(result);
+
+    }
+
+    private static int find(int[] a) {
+        if(a == null && a.length < 1){
+            return  Integer.MAX_VALUE;
+        }
+        int count = 0;
+        int temp = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (count == 0) {
+                temp = a[i];
+                count++;
+            } else {                                   //如何把list中得到的元素转为int
                 temp = (temp == a[i] ? count++ : count--);
             }
         }
@@ -35,7 +92,9 @@ public class FindHalfNum {
                 count++;
             }
         }
-        return count >= (a.length / 2 + 1) ? temp : 0;
+        if (count >= a.length / 2) {
+            return temp;
+        }
+        return Integer.MAX_VALUE;
     }
-
-}
+}*/
--- 题集/src/question_array/NiXuDui.java
+++ 题集/src/question_array/NiXuDui.java
@@ -1,5 +1,17 @@
 package question_array;
 
+/**
+ * 链接：https://www.nowcoder.com/questionTerminal/bb06495cc0154e90bbb18911fd581df6
+ * 来源：牛客网
+ *
+ * 有一组数，对于其中任意两个数组，若前面一个大于后面一个数字，则这两个数字组成一个逆序对。请设计一个高效的算法，计算给定数组中的逆序对个数。
+ *
+ * 给定一个int数组A和它的大小n，请返回A中的逆序对个数。保证n小于等于5000。
+ *
+ * 测试样例：
+ * [1,2,3,4,5,6,7,0],8
+ * 返回：7
+ */
 public class NiXuDui {
     public int inverse(int[] array){
         if(array == null || array.length == 0){
